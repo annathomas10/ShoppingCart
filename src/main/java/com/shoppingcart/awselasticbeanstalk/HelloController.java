3c831bfb8cb55c510f88d0b66970e97800789632
anna thomas <zarafa000@yahoo.com>
2024-05-03 18:50:52 -0400

    files not added when done thru terminal...

--- src/main/java/com/shoppingcart/awselasticbeanstalk/HelloController.java
+++ src/main/java/com/shoppingcart/awselasticbeanstalk/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     public String Hello(){
-        return "Hello World";
+        return "Hello youtube";
     }
 }
